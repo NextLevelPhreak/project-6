62da2d831de97f4be9f9ac228be16e497b7dc0e5
Austin Baxter <austinbaxter6202@outlook.com>
2021-06-10 14:32:32 -0700

    project 6
    updated javascript

--- js/app.js
+++ js/app.js
@@ -1,11 +1,9 @@
 const keyboard = document.querySelector('#qwerty');
-const phrases = document.querySelector('#phrase');
-const overlay = document.querySelector('#overlay');
-const buttonReset = document.getElementsByClassName('.btn__reset');
+const phrase = document.querySelector('phrase');
+const buttonReset = document.querySelector('.btn__reset');
 let missed = 0;
-let matches = [];
 
-const phrases = [
+let phrases = [
   'oceans are huge',
   'javascript is complex',
   'treehouse is fun',
@@ -13,29 +11,17 @@ const phrases = [
   'technology is the future',
 ];
 
+const overlay = document.querySelector('#overlay');
 buttonReset.addEventListener('click', () => {
   overlay.style.display = 'none';
-  reset();
 });
 
 /* this will choose a random phrase from the array */
 
 function getRandomPhraseAsArray(arr) {
-  let randomNumber = Math.floor(Math.random() * arr.length)
-  return arr[randomNumber];
+  let randomNumber = Math.floor(Math.random() * 5);
+  let randomArr =arr[randomNumber()];
+  return splitArray = randomArr.split('');
 }
 
 /* adds the phrase to the display */
-
-function addPhraseToDisplay(arr) {
-  arr.forEach(item => {
-    let listElement = document.createElement('li');
-    listElement.textContent = item;
-    if (item !== ' '){
-        listElement.classList.add('letter');
-    } else {
-        listElement.classList.add('space');
-      }
-phrases.querySelector('ul').append(listElement);
-  });
-}
